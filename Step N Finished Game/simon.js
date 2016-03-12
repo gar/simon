4971aa6407bca6384c0507feb222c888779b167f
Gar Morley <garmorley@gmail.com>
2016-03-12 11:06:24 +0000

    Flash boxes when you click on them

--- Step N Finished Game/simon.js
+++ Step N Finished Game/simon.js
@@ -1,3 +1,10 @@
-$(function() {
+var flashBox = function(boxId) {
+  $(boxId).fadeOut().fadeIn();
+}
 
+$(function() {
+  $(".box").on("click", function(event) {
+    var boxId = "#"+event.target.id;
+    flashBox(boxId);
+  });
 });
